3bdf7c1287afd6b9c0663550acbc29b4cfd14093
shubhangi kore <koreshubhangi291@gmail.com>
2025-09-20 23:55:11 +0530

    Updated port

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:4001",
+        target: "http://localhost:4002",
         changeOrigin: true,
       },
     },
